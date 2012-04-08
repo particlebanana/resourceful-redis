536aae4345c7ff31793f5f89387d3ab8f3304a21
Cody Stoltman <particlebanana@gmail.com>
2012-04-08 17:24:50 -0500

    add an example

---
+++ examples/creature.js
@@ -0,0 +1,28 @@
+var resourceful = require('../lib/resourceful-redis');
+
+var Creature = resourceful.define('creature', function () {
+
+  // Specify redis engine and connection
+  this.use("redis", {
+    uri: "redis://DB:Pass@127.0.0.1:6379" // Set connection string here, auth is optional
+  });
+
+  this.string('diet');
+  this.bool('vertebrate');
+  this.array('belly');
+
+  this.timestamps();
+
+  this.prototype.feed = function (food) {
+    this.belly.push(food);
+  };
+});
+
+var wolf = new(Creature)({
+  diet:      'carnivore',
+  vertebrate: true
+});
+
+console.dir(wolf.belly);
+wolf.feed('squirrel');
+console.dir(wolf.belly);
